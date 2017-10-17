88f24e9800b7aa8bef81570f47c90ffd940e0e46
Jon Camerer <jon.camerer@gmail.com>
2017-10-17 18:31:00 -0500

    Add posts form and validations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,15 +2,27 @@ class PostsController < ApplicationController
   before_action :logged_in_user, only: [:new, :edit]
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+
+    if @post.update_attribute(:user_id, current_user.id)
+      redirect_to root_url
+    else
+      render 'new'
+    end
   end
 
   def index
   end
 
   private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+
     def logged_in_user
       unless logged_in?
         redirect_to login_url
